1927b7f0a6974907a49f4bdcd3a47fea7485b319
mmarhalim <m.marhalim@gmail.com>
2023-02-27 15:10:51 -0700

    added DOM manipulation by fetching from themoviedb.org

--- index.js
+++ index.js
@@ -1,4 +1,66 @@
 const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=67ad4844d9f1f80402541f22f8983508&page=1';
-const IMG_PATH = '';
-const SEARCHAPI = '';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=67ad4844d9f1f80402541f22f8983508&query=";
+
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
+
+$(document).ready(function() {
+  function returnMovies(url) {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
+        showMovies(data);
+      });
+  }
+
+  function showMovies(movies) {
+    movies.results.forEach(element => {
+      const div_card = document.createElement("div");
+      div_card.setAttribute("class", "card");
+
+      const div_row = document.createElement("div");
+      div_row.setAttribute("class", "row");
+
+      const div_column = document.createElement("div");
+      div_column.setAttribute("class", "column");
+
+      const image = document.createElement("img");
+      image.setAttribute("class", "thumbnail");
+      image.setAttribute("id", "image")
+
+      const title = document.createElement("h3");
+      title.setAttribute("id", "title");
+
+      const center = document.createElement("center");
+
+      title.innerHTML = element.title;
+      image.src = IMG_PATH + element.poster_path;
+
+      center.appendChild(image);
+      div_card.appendChild(center);
+      div_card.appendChild(title);
+      div_column.appendChild(div_card);
+      div_row.appendChild(div_column);
+      main.appendChild(div_row);
+    })
+  }
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = "";
+
+    let searchValue = search.value;
+
+    if (searchValue) {
+      returnMovies(SEARCHAPI + searchValue);
+      search.value = "";
+    }
+  });
+
+  returnMovies(APILINK);
+
+});
 
